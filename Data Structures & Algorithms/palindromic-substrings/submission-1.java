701c29d2228c67a23ed2bf8bd151d48a95985be8
92patilshailesh <92patilshailesh@gmail.com>
2026-04-22 14:49:12 -0700

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    //O(n^2)
+    public int countSubstrings(String s) {
+        int res = 0;
+        for(int i = 0; i< s.length(); i++) {
+            res += countPalis(s, i,i);
+            res+= countPalis(s, i ,i+1);
+        }
+        return res;
+        
+    }
+    private int countPalis(String s, int l, int r) {
+        int res = 0;
+        while(l >=0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            res++;
+            l--;
+            r++;
+        }
+        return res;
+    }
+}
